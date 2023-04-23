a7110e1dcba30ad0ff6bebb9129ceb09db512e37
Terry <68700354+teraprath@users.noreply.github.com>
2023-04-24 01:22:09 +0200

    UPDATE Spectator Mode

---
+++ src/main/java/dev/teraprath/gamelib/src/main/java/dev/teraprath/gamelib/event/RunningTimerRunEvent.java
@@ -0,0 +1,35 @@
+package dev.teraprath.gamelib.event;
+
+import dev.teraprath.gamelib.game.Game;
+import dev.teraprath.gamelib.timer.RunningTimer;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nonnull;
+
+public class RunningTimerRunEvent extends Event {
+
+    private static final HandlerList HANDLER_LIST = new HandlerList();
+    private final RunningTimer timer;
+    private final Game game;
+
+    public RunningTimerRunEvent(@Nonnull RunningTimer timer, @Nonnull final Game game) {
+        this.timer = timer;
+        this.game = game;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return HANDLER_LIST;
+    }
+
+    public static HandlerList getHandlerList() { return HANDLER_LIST; }
+
+    public RunningTimer getTimer() {
+        return this.timer;
+    }
+
+    public Game getGame() { return this.game; }
+
+}
---
+++ src/main/java/dev/teraprath/gamelib/src/main/java/dev/teraprath/gamelib/utils/ServerUtils.java
@@ -0,0 +1,85 @@
+package dev.teraprath.gamelib.utils;
+
+import dev.teraprath.gamelib.game.Game;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Properties;
+
+public class ServerUtils {
+
+    private final JavaPlugin plugin;
+    private final Game game;
+    private Field maxPlayersField;
+
+
+    public ServerUtils(JavaPlugin plugin, Game game) {
+        this.plugin = plugin;
+        this.game = game;
+    }
+
+    public void changeSlots(int slots) throws ReflectiveOperationException {
+        Method serverGetHandle = plugin.getServer().getClass().getDeclaredMethod("getHandle");
+        Object playerList = serverGetHandle.invoke(plugin.getServer());
+
+        if (this.maxPlayersField == null) {
+            this.maxPlayersField = getMaxPlayersField(playerList);
+        }
+
+        this.maxPlayersField.setInt(playerList, slots);
+        updateServerProperties();
+    }
+
+
+    private Field getMaxPlayersField(Object playerList) throws ReflectiveOperationException {
+        Class<?> playerListClass = playerList.getClass().getSuperclass();
+
+        try {
+            Field field = playerListClass.getDeclaredField("maxPlayers");
+            field.setAccessible(true);
+            return field;
+        } catch (NoSuchFieldException e) {
+            for (Field field : playerListClass.getDeclaredFields()) {
+                if (field.getType() != int.class) {
+                    continue;
+                }
+
+                field.setAccessible(true);
+
+                if (field.getInt(playerList) == plugin.getServer().getMaxPlayers()) {
+                    return field;
+                }
+            }
+
+            throw new NoSuchFieldException("Unable to find maxPlayers field in " + playerListClass.getName());
+        }
+    }
+
+    public void updateServerProperties() {
+        Properties properties = new Properties();
+        File propertiesFile = new File("server.properties");
+
+        try {
+            try (InputStream is = new FileInputStream(propertiesFile)) {
+                properties.load(is);
+            }
+
+            String maxPlayers = Integer.toString(plugin.getServer().getMaxPlayers());
+
+            if (properties.getProperty("max-players").equals(maxPlayers)) {
+                return;
+            }
+
+            properties.setProperty("max-players", maxPlayers);
+
+            try (OutputStream os = new FileOutputStream(propertiesFile)) {
+                properties.store(os, "Minecraft server properties");
+            }
+        } catch (IOException e) {
+            plugin.getLogger().warning("An error occurred while updating the server properties");
+        }
+    }
+
+}
